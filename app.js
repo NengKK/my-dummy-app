982349498a8676a7cbc172733f9cc307c0eeac95
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2019-11-14 11:01:00 +0700

    Update project same as aws tutorial

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-var express = require('express');
-var app = express();
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('index.html');
 
-app.get('/', (req, res) => {
-    res.send('Hello, from AWS elastic beanstalk!');
+var log = function(entry) {
+    fs.appendFileSync('/tmp/my-dummy-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
+
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('end', function() {
+            if (req.url === '/') {
+                log('Received message: ' + body);
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
+
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        res.writeHead(200);
+        res.write(html);
+        res.end();
+    }
 });
 
-app.listen(3000, () => {
-    console.log('Application started!');
-});
+server.listen(port);
+console.log('Server running at http://127.0.0.1:' + port + '/');
