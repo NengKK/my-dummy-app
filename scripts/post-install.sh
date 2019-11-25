07aa1326d64e8f0d1687cf8cf6ad2531e88686d3
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2019-11-25 14:58:34 +0700

    Use CIRCLE_BRANCH variable

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -2,13 +2,13 @@
 
 echo "HELLO, FROM POST-INSTALL"
 
-if [ "$ENV" = "PROD" ]; then
-    echo "PRODUCTION"
+if [ "$CIRCLE_BRANCH" = "production" ]; then
+    echo "$CIRCLE_BRANCH"
 	npm install git+ssh://git@github.com/NengKK/dummy-package.git#production
-elif [ "$ENV" = "POC" ]; then
-    echo "POC"
+elif [ "$CIRCLE_BRANCH" = "test-branch-changing" ]; then
+    echo "$CIRCLE_BRANCH"
 	npm install git+ssh://git@github.com/NengKK/dummy-package.git#poc
 else
-    echo "MASTER"
+    echo "$CIRCLE_BRANCH"
 	npm install git+ssh://git@github.com/NengKK/dummy-package.git
 fi
