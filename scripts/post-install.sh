6f386868cdfd1ced54966d31f8fac3a69e8f404f
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2019-11-26 09:12:37 +0700

    Start to test again

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -4,11 +4,11 @@ echo "HELLO, FROM POST-INSTALL"
 
 if [ "$CIRCLE_BRANCH" = "production" ]; then
     echo "$CIRCLE_BRANCH"
-	npm install git+ssh://git@github.com/NengKK/dummy-package.git#production
+	# npm install git+ssh://git@github.com/NengKK/dummy-package.git#production
 elif [ "$CIRCLE_BRANCH" = "test-branch-changing" ]; then
     echo "$CIRCLE_BRANCH"
-	npm install git+ssh://git@github.com/NengKK/dummy-package.git#poc
+	# npm install git+ssh://git@github.com/NengKK/dummy-package.git#poc
 else
     echo "$CIRCLE_BRANCH"
-	npm install git+ssh://git@github.com/NengKK/dummy-package.git
+	# npm install git+ssh://git@github.com/NengKK/dummy-package.git
 fi
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 require('dotenv').config();
 import express = require('express');
-import { ShowBranch } from 'dummy-package';
+// import { ShowBranch } from 'dummy-package';
 
 const port = process.env.PORT || '3000';
 const app: express.Application = express();
 
 app.get('/', (req, res) => {
-    res.send(`on ${ShowBranch.BRANCH} branch`);
+    // res.send(`on ${ShowBranch.BRANCH} branch`);
+    res.send(`ok`);
 });
 
 app.listen(port, () => {
-    console.log(`App listening on '${ShowBranch.BRANCH}' branch on port ${port}`);
+    // console.log(`App listening on '${ShowBranch.BRANCH}' branch on port ${port}`);
+    console.log(`App listening port ${port}`);
 });
