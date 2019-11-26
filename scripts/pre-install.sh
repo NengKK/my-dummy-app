24b611fc01f3c2c25c745a70b84cfd04125d4e3f
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2019-11-26 18:02:23 +0700

    Change message in preinstall script

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -3,12 +3,12 @@
 echo "HELLO, FROM PREINSTALL PHASE"
 
 if [ "$STAGE" = "production" ]; then
-    echo stage="$STAGE"
+    echo "stage=production"
     sed -i -e 's/dummy-package.git/dummy-package.git#production/g' ./package.json
 elif [ "$STAGE" = "test" ]; then
-    echo stage="$STAGE"
+    echo "stage=test"
     sed -i -e 's/dummy-package.git/dummy-package.git#poc/g' ./package.json
 else
-    echo stage="$STAGE"
+    echo "stage=rc or latest"
     sed -i -e 's/dummy-package.git/dummy-package.git/g' ./package.json
 fi
