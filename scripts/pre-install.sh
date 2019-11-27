f9680ce16298ac16d7f64c7c54228dc45e3aa9be
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2019-11-27 15:15:08 +0700

    Test

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -2,9 +2,9 @@
 
 echo "HELLO, FROM PREINSTALL PHASE"
 
-if [ "$STAGE" = "production" ]; then
+if [ "${STAGE}" = "production" ]; then
     sed -i -e 's/winston.git/winston.git#winston1/g' ./package.json
-elif [ "$STAGE" = "test" ]; then
+elif [ "${STAGE}" = "test" ]; then
     sed -i -e 's/winston.git/winston.git#winston2/g' ./package.json
 else
     sed -i -e 's/winston.git/winston.git/g' ./package.json
