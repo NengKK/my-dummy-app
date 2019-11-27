07d8c0f66d16a0e2315d869ab5bec590b9eb73dd
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2019-11-27 09:30:08 +0700

    Test change permission

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -3,12 +3,9 @@
 echo "HELLO, FROM PREINSTALL PHASE"
 
 if [ "$STAGE" = "production" ]; then
-    echo "stage=production"
     sed -i -e 's/dummy-package.git/dummy-package.git#production/g' ./package.json
 elif [ "$STAGE" = "test" ]; then
-    echo "stage=test"
     sed -i -e 's/dummy-package.git/dummy-package.git#poc/g' ./package.json
 else
-    echo "stage=rc or latest"
     sed -i -e 's/dummy-package.git/dummy-package.git/g' ./package.json
 fi
