04ff49d038ff31bbf1d9fd74340e39611d5e5720
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2019-11-27 23:17:16 +0700

    Help me!

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -2,10 +2,10 @@
 
 echo "HELLO, FROM PREINSTALL PHASE"
 
-if [ "${STAGE}" = "production" ]; then
-    sed -i -e 's/winston.git/winston.git#winston1/g' ./package.json
-elif [ "${STAGE}" = "test" ]; then
-    sed -i -e 's/winston.git/winston.git#winston2/g' ./package.json
-else
-    sed -i -e 's/winston.git/winston.git/g' ./package.json
-fi
+# if [ "${STAGE}" = "production" ]; then
+#     sed -i -e 's/winston.git/winston.git#winston1/g' ./package.json
+# elif [ "${STAGE}" = "test" ]; then
+#     sed -i -e 's/winston.git/winston.git#winston2/g' ./package.json
+# else
+#     sed -i -e 's/winston.git/winston.git/g' ./package.json
+# fi
