ab8b9d2860a6e64e7a9e2f0a8ef02abc9748aa11
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2019-11-28 14:49:33 +0700

    Add print environment variable

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+echo STAGE=$STAGE
+
 if [ "${STAGE}" = "production" ]; then
-    sed -i -e 's/dummy-package.git/dummy-package.git/g' ./package.json;
+    sed -i -e 's/dummy-package.git/dummy-package.git#production/g' ./package.json;
 elif [ "${STAGE}" = "test" ]; then
     sed -i -e 's/dummy-package.git/dummy-package.git#poc/g' ./package.json;
 else
-    sed -i -e 's/dummy-package.git/dummy-package.git#production/g' ./package.json;
+    sed -i -e 's/dummy-package.git/dummy-package.git/g' ./package.json;
 fi
