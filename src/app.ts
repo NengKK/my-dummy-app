8f91245f2b94f91af3eb0c52c0ec2f7d599700ae
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2019-11-28 10:11:50 +0700

    Change to custom package

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 require('dotenv').config();
 import express = require('express');
+import { ShowBranch } from 'dummy-package';
 
 const port = process.env.PORT || '3000';
 const app: express.Application = express();
 
 app.get('/', (req, res) => {
-    res.send(`Hello`);
+    res.send(`Hello from branch "${ShowBranch.BRANCH}"`);
 });
 
 app.listen(port, () => {
