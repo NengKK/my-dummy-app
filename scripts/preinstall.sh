bb4a646ff069b7cb80c250f8a3627124ab9b1f98
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2019-11-28 14:59:42 +0700

    Set AWS EB branch environment

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-echo STAGE=$STAGE
+echo CIRCLE_BRANCH=$CIRCLE_BRANCH
 
-if [ "${STAGE}" = "production" ]; then
+if [ "${CIRCLE_BRANCH}" = "production" ]; then
     sed -i -e 's/dummy-package.git/dummy-package.git#production/g' ./package.json;
-elif [ "${STAGE}" = "test" ]; then
-    sed -i -e 's/dummy-package.git/dummy-package.git#poc/g' ./package.json;
-else
+elif [ "${CIRCLE_BRANCH}" = "master" ]; then
     sed -i -e 's/dummy-package.git/dummy-package.git/g' ./package.json;
+else
+    sed -i -e 's/dummy-package.git/dummy-package.git#poc/g' ./package.json;
 fi
